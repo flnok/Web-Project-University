388500eb063bf7546076a8276b55606ac542416b
Nguyen Huu Canh <flnok@flnok.local>
2019-09-24 03:57:29 +0700

    80% Hoàn thành trang chủ ( đã chỉnh responsive )
    Những việc chưa làm:
    Làm trang login, trang quản lý, trang About, trang Room Detail, trang Offers.
    Cần thêm ít nhất 4 phòng:
    2 phòng đơn, 1 phòng đôi, 1 phòng tập thể.
    Phòng đơn có 1 giường 18x20
    Phòng đôi có 2 giường 16x20
    Phòng tập thể có 3 giường: 1 giường 16x20, 2 giường 18x20.
    .
    Các dịch vụ cần thêm: Buffet, cho thuê xe, ưa đãi cho nhóm 4 người.
    .
    Giá các loại phòng lần lượt: 600k, 800k, 1200k
    .
    Mày xem điền thông tin vào mấy chổ t ghi tùm lum kia giùm tao nha, tiếng anh tiếng việt đều được.

---
+++ js/app.js
@@ -0,0 +1,117 @@
+const on_click = () => {
+    const menu = document.querySelector('.nav-menu');
+    const menu_links = document.querySelector('.nav-links');
+    const menu_items = document.querySelectorAll('.nav-links li');
+    const navitems = document.querySelector('.navigation');
+    const nav_items = document.querySelectorAll('.navigation .bar');
+
+
+
+    menu.addEventListener('click', () => {
+        //toggle nav
+        menu_links.classList.toggle('nav-active');
+        //animate
+        menu_items.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `animate-nav-menu 1.5s ease ${index/7
+                    + 0.3}`;
+            }
+        });
+    });
+};
+
+const app = () => {
+    on_click();
+};
+
+app();
+////////////////////////////
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('#button-scroll-top').fadeIn('slow');
+        } else {
+            $('#button-scroll-top').fadeOut('slow');
+        }
+    });
+
+    $("#button-scroll-top").click(function () {
+        $('html ,body').animate({
+            scrollTop: 0
+        }, 800);
+    });
+});
+// ////////////////////////////
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("review-slides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function changeNumber(obj) {
+
+    if (obj.id == "downR" || obj.id == "upR") {
+        var numberRoom = document.getElementById("number_room");
+        var value = parseInt(numberRoom.value);
+        if (obj.id == "downR") {
+            value--;
+        } else {
+            value++;
+        }
+        if (value < 1) {
+            value = 1;
+        } else if (value > 10) {
+            value = 10;
+        }
+        numberRoom.value = value;
+    } else if (obj.id == "downG" || obj.id == "upG") {
+        var numberGuest = document.getElementById("number_guest");
+        var value = parseInt(numberGuest.value);
+        if (obj.id == "downG") {
+            value--;
+        } else {
+            value++;
+        }
+        if (value < 1) {
+            value = 1;
+        }
+        numberGuest.value = value;
+    }
+}
+
+// Google Map
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(10.762622, 106.660172),
+        zoom: 12,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
